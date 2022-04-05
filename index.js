b6155b11058c50845723d468197213eb60d98e69
fabien desnoues <f.desnoues@keabot.fr>
2022-04-05 17:25:03 +0200

    scrapping lafayette pharma + create file JSON

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra')
+const fs = require('fs');
+
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+let nbPage = '3';
+let category = "veterinaire";
 
 (async () => {
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
-    await page.goto('https://www.imdb.com/movies-coming-soon/2022-07');
-    const movies = await page.evaluate(() => {
-        let movies = [];
-        let list = document.querySelectorAll("#main > div > div.list.detail > div.list_item");
-        for (elem of list) {
-            movies.push({
-                img: elem.querySelector("img").src,
-                title: elem.querySelector("h4 > a").innerText,
-            })
-        }
-        return movies;
-    });
-    console.log(movies);
+    let nb = "1";
+    while (nb <= nbPage) {    
+    console.log("page : " + nb);    
+        await page.goto(`https://www.parapharmacielafayette.com/fr/cp/${category}.html?page=${nb}`);
+        const choco = await page.evaluate(() => {
+            let products = [];
+            let list = document.querySelectorAll("#listing > article");
+            for (elem of list) {
+                products.push({
+                    "brand": elem.querySelector("h3 > a > div")?.innerText,
+                    "lien": elem.querySelector("h3 > a")?.href,
+                    "img": elem.querySelector("img")?.src,
+                })
+            }
+            return products;
+        });
+        createJSON(choco);
+         nb++; 
+    }
     await browser.close();
     
 })();
+
+function createJSON(products) {
+    let json = JSON.stringify(products);
+    fs.writeFileSync(`./${category}.json`, json);
+}
